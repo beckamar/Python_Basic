7d8f828d489d0b7c6947286cfabd853055a075fa
Rebeca Marcial <rebecamarcial648@gmail.com>
2022-07-11 17:27:14 -0500

    Determinar palabras y frases palindromas

--- basic/practices.py
+++ basic/practices.py
@@ -1,45 +1,21 @@
-
-def converter (type_peso, value_Dollar):
-
-    peso = int (input("Escribe los pesos "+ type_peso + " que tienes: $" ))
+def determine(text):
+    text = text.strip().replace(' ','')
+    text_palindrome = text[::-1]
     
-    dollars = peso / value_Dollar
-    dollars = round(dollars, 2)
+    if text == text_palindrome:
+        print("Is a palindrome")
     
-    dollars = str(dollars)
-    print("Tienes $"+dollars + " dolares") 
+    else:
+        print("Not a palidrome")
+
+
+def information():
+    text = str(input("Write a word or a phrase: "))
+    determine(text)
+
+
+if __name__ == "__main__":
+    information()
 
 
 
-
-menu = """
-
-Bienvenido al conversor de monedad
-
-1. Pesos mexicanos
-2. Pesos argentenios
-3. Pesos colombianos
-
-"""
-opcion = int(input(menu))
-
-if opcion == 1:
-    converter("mexicanos", 20.53)
-elif opcion == 2:
-    converter("argentinos",127.33)
-elif opcion == 3:
-    converter("colombianos",4450.24)
-
-
-
-
-#convertidor de dolares a pesos
-
-#dollar = int (input("Escribe los dolares: "))
-#value_Peso = .049
-
-#pesos = dollar / value_Peso
-#pesos = round(pesos,2)
-#pesos = str(pesos)
-#
-#print("Tienes $"+ pesos + " pesos")
