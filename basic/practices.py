0bac03cd04dcef8a0015c62dcb8c77d811776f6b
Rebeca Marcial <rebecamarcial648@gmail.com>
2022-07-11 16:38:02 -0500

    Slices o rebanadas

--- basic/practices.py
+++ basic/practices.py
@@ -20,7 +20,7 @@ name = name.upper()
 #Convierte primer caracter en mayuscula
 name = name.capitalize()
 
-#Elimina espacios 
+#Elimina espacios basura
 name = name.strip()
 print(name)
 
@@ -35,3 +35,40 @@ print(name)
 #Muestra la letra del indice que indicamos
 name = name [0]
 print(name)
+
+#Dice la cantidad de caracteres que tiene la cadena
+len(name)
+
+
+
+
+#---------------------------SLICES----------------------------#
+#-----------------O rebenadas
+
+name = "Francisco"
+
+#Imprime el caracter de la posicion que le indiquemos, en este caso 0
+print(name[0])
+
+#imprime los caracteres de la posicion 0 a 3
+print(name[0:3])
+#Es lo mismo que 
+print(name[:3])
+# O tambien: imprime de la posicion al final
+print(name[3:])
+#En medio o desde donde sea
+print(name[1:7])
+
+
+#Tambien se puede emprimir desde un incide a otro haciendo saltos, 
+#en este caso de 2 en 2 y solo marcará los caracteres en los que caiga
+print(name[1:7:2])
+#O tambien
+print(name[::])
+print(name[1::2])
+
+
+#O podemos imprimir la cadena al reves con
+print(name[::-1])
+
+
