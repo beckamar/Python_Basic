2ac782f137f2ece260b1910212a573cb7fcdd751
Rebeca Marcial <rebecamarcial648@gmail.com>
2022-07-12 12:21:48 -0500

    Ciclo while para potencia

--- basic/practices.py
+++ basic/practices.py
@@ -1,12 +1,31 @@
 
-def run(num, rept):
-    if num <= rept:
-        cont = num
-        print(str(2 ** cont) )
-        run(num+1, rept)
-    else:
-        print("Fin!")
+def run():
+
+    LIMITE = 1000
+    contador = 0
+    potencia_2 = 2**contador
+
+    while potencia_2 < LIMITE:
+        print(str(potencia_2))
+        contador = contador +1
+        potencia_2 = 2**contador
+
 
 if __name__ == "__main__":
-    repeticiones = int(input("Cuantas potencias: "))
-    run(0, repeticiones)
+    run()
+
+
+
+
+
+#def run(num, rept):
+#    if num <= rept:
+#        cont = num
+#        print(str(2 ** cont) )
+#        run(num+1, rept)
+#    else:
+#        print("Fin!")
+#
+#if __name__ == "__main__":
+#    repeticiones = int(input("Cuantas potencias: "))
+#    run(0, repeticiones)
