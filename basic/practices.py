05fcfc0fca5a257fa86bbbb64760b133ba990676
Rebeca Marcial <rebecamarcial648@gmail.com>
2022-07-13 15:55:23 -0500

    Manejando lo basico de estructura de datos

--- basic/practices.py
+++ basic/practices.py
@@ -1,3 +1,44 @@
+#Las llaves {} sirven para definir diccionarios
+#Un diccionario es una estructura de datos de llaves y valores
+
+def run():
+    mi_diccionario = {
+        'llave1': 1,
+        'llave2': 2,
+        'llave3': 3,
+    }
+
+    poblacion_paises = {
+        'Argentina': 44938712,
+        'Brasil': 210147125,
+        'Colombia': 50372424
+    }
+
+    # print(poblacion_paises['Bolivia'])
+
+    # for pais in poblacion_paises.keys():
+    #     print(pais)
+
+    # for pais in poblacion_paises.values():
+    #     print(pais)
+
+    for pais, poblacion in poblacion_paises.items():
+        print(pais + ' tiene ' + str(poblacion) + ' habitantes')
+
+'''
+.keys():Retorna la clave de nuestro elemento.
+
+.values(): Retorna una lista de elementos (valores del diccionario).
+
+.items(): Devuelve lista de tuplas (primero la clave y luego el valor).
+
+.clear(): Elimina todos los items del diccionario.
+
+.pop(“n”): Elimina el elemento ingresado.
+
+'''
+
 
 if __name__ == '__main__':
-    print("Holiwi perros")
+    run()
+
