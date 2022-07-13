b2643ff80204b959913d58e8e3aa6ef126cf30b9
Rebeca Marcial <rebecamarcial648@gmail.com>
2022-07-13 11:57:05 -0500

    Declarar, llenar, recorrer y demas funciones en listas

--- basic/practices.py
+++ basic/practices.py
@@ -1,44 +1,112 @@
-#(n-1)! + 1 es múltiplo de n
 
-def fac(number):
-    
-    factorial = 1
-    for i in range(1, number):
-        factorial *= i
-    
-    factorial += 1
-    return factorial
+numeros = [1, 2, 3, 4, 5, 6]
+objetos = ["Maria", 34, 7.5, True, ]
+
+#Para acceder a ellos es colocando el indice dentro de los corchetes
+numeros[5]
+objetos[3]
+
+#Para agregar un nuevo valor a la lista: Se coloca dentro de los parentesis
+numeros.append(2)
+objetos.append(False)
+
+#Mostrar las listas completas
+numeros
+objetos
+
+#Eliminar un valor dentro de la lista: Se coloca el indie del elemento
+numeros.pop(4)
+objetos.pop(1)
+
+#Recorrer la lista en orden
+for i in numeros:
+    print(i)
+
+for i in objetos:
+    print(i)
+
+#Recorrer la lista al reves
+numeros[::-1]
+objetos[::-1]
+
+#Ir de un elemento a otro, ejemplo: ir del elemento en el indice 1 al 3
+numeros[1:3]
+objetos[1:3]
 
 
-def determine():
 
-    number = int(input("Escribe un numero: "))
-    if number > 1:
-      if fac(number) % (number):
-          print("No es primo")
-      else:
-          print("Es primo")
-    else:
-        print()
+#-------------------------- OTRAS FORMAS --------------------------
 
+#Declar lista
+my_lista = []
+my_lista = list()
 
+#Unir listas
+my_lista = [1]
+my_lista2 = [2,3,4]
+my_lista3 = my_lista + my_lista2
+my_lista3 # [1,2,3,4]
 
-if __name__ == "__main__":
-    determine()
+#Partir listas como slices
+my_lista = [1,2,3]
+my_lista[1:] # = [2,3]
 
+#Extender una lista
+my_lista = [1]
+my_lista2 = [2,3,4]
+my_lista.extend(my_lista2) # [1,2,3,4]
 
+#Multiplicar listas
+my_lista = ['a']
+my_lista2 = my_lista * 5
+my_lista2 # ['a','a','a','a','a']
 
+#Eliminar el último elemento de la lista
+my_lista = [1,2,3,4,5]
+my_lista = my_lista.pop()
+my_lista # [1,2,3,4]
 
+#Ordenar lista
+my_lista = [2,1,5,4,3]
+my_lista = my_lista.sort()
+my_lista # [1,2,3,4,5]
 
-#def run():
-#    number = int(input('Escribe un numero: '))
-#    print('Es primo'if (isPrime(number)) else'No es primo')
-#
-#def isPrime(num):
-#    return ((factorial(num - 1) + 1) % num) == 0ifnum > 1else False
-#
-#def factorial(num):
-#    return1ifnum <= 1else (num * factorial(num - 1))
-#
-#if __name__ == '__main__':
-#    run()
+#Eliminar un elemento
+my_lista = [1,2,3,4,5]
+del my_lista[0]
+my_lista # [2,3,4,5]
+
+#Eliminar si conocemos su valor
+my_lista = [1,2,3,4,5]
+my_lista.remove(3)
+my_lista #[1,2,4,5]
+
+#saber qué métodos hay dentro de un elemento
+dir(my_lista) # ['__add__', '__class__', '__contains__', ...
+
+#Modificar un elemento
+my_lista = [1,2,3,4,5]
+my_lista[0] = 100
+my_lista # [100,2,3,4,5]
+
+#Añadir un elemento al final
+my_lista = [1,2,3,4,5]
+my_lista.append(6)
+my_lista # [1,2,3,4,5,6]
+
+'''
+Métodos adicionales para listas
+.sorted()
+También ordena como sort() pero modifica la lista inicial
+.clear()
+Vacía la lista
+.count()
+Cuenta las veces que esta un elemento en lista
+.index()
+Indica la posición donde esta un elemento de la lista
+.insert()
+Inserta un elemento en una posición dada ej: lista.insert(posición,item)
+.reverse()
+Le da la vuelta a una lista
+
+'''
